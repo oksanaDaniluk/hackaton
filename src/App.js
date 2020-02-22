9996a2138bf152b8eeac976e66654a78b03ee81e
oksanaDaniluk <oksana.daniluk@ukr.net>
2020-02-23 00:38:03 +0200

    add todo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import './App.scss';
-import { MoviesList } from './components/MoviesList';
-import moviesFromServer from './api/movies.json';
+import { AddTodo } from './components/TaskList/AddTodo';
 
 export class App extends Component {
-  state = {};
+  state = {
+    isTaskCreator: false,
+  };
+
+  loadTaskCreator = () => {
+    this.setState({
+      isTaskCreator: true,
+    });
+  }
 
   render() {
     return (
-
+      <div className="App">
+        {
+          this.state.isTaskCreator
+            ? (
+              <AddTodo />
+            )
+            : (
+              <button
+                className="button button__task-creator"
+                type="button"
+                onClick={this.loadTaskCreator}
+              >
+                Add task
+              </button>
+            )
+        }
+      </div>
     );
   }
 }
